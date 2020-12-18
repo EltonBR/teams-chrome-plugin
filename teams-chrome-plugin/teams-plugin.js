d5dae64faf4d8e01dee6d06852ea57d5d165c986
Elton John <37675998+EltonBR@users.noreply.github.com>
2020-12-18 00:48:24 -0300

    Add files via upload

---
+++ teams-chrome-plugin/teams-plugin.js
@@ -0,0 +1,96 @@
+class MicIndicator {
+    divRef = null;
+    interval = null;
+    constructor() {
+        let div = document.createElement("div");
+        div.id = "div-animated-indicator";
+        this.divRef = div;
+        document.body.appendChild(div)
+    }
+
+    show() {
+        if (this.divRef !== null) {
+            this.divRef.style.display = "block";
+        }
+    }
+
+    hide() {
+        if (this.divRef !== null) {
+            this.divRef.style.display = "none";
+        }
+    }
+
+    watchMicChanges() {
+        this.interval = setInterval(() => {
+            let micBtn = document.querySelector(".icons-call-microphone-off-filled") ?? document.querySelector(".icons-call-microphone")
+            if (micBtn === null) {
+                this.hide();
+                return;
+            }
+            let isMuted = [...micBtn.classList].indexOf("icons-call-microphone-off-filled") > -1;
+            if (!isMuted) {
+                this.show();
+            } else {
+                this.hide();
+            }
+        }, 500);
+    }
+
+    stopMicChanges() {
+        if(this.interval !== null) {
+            clearInterval(this.interval);
+        }
+    }
+} 
+
+var isPressed = false;
+
+const pushToTalkEvent = (e) => {
+    if (e.code !== 'AltRight') {
+        return;
+    }
+
+    if (isPressed && e.type == "keydown") {
+        return;
+    }
+
+    console.log()
+
+    const mute = (mute) => {
+        var micBtn = document.querySelector(".icons-call-microphone-off-filled") ?? document.querySelector(".icons-call-microphone")
+        isMuted = [...micBtn.classList].indexOf("icons-call-microphone-off-filled") > -1;
+        if (mute) {
+            if (isMuted) {
+                return false;
+            }
+            micBtn.parentElement.click();
+            return true;
+        }
+        if (!isMuted) {
+            return false;
+        }
+        micBtn.parentElement.click();
+        return true;
+    };
+
+    if (e.type == "keydown") {
+        mute(false);
+    } else if (e.type == "keyup") {
+        mute(true);
+    }
+
+    //dry
+    if(e.type == "keydown") {
+        isPressed = true;
+    } else {
+        isPressed = false;
+    }
+};
+
+window.addEventListener("load", () => {
+    micIndicatorInstance = new MicIndicator();
+    micIndicatorInstance.watchMicChanges();
+    window.addEventListener("keydown", pushToTalkEvent)
+    window.addEventListener("keyup", pushToTalkEvent)
+});
+
